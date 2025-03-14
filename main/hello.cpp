01a99de685adb53eab20c948af7d95babd9b8274
ROHITH  SUDHAKAR <rohithsudhakarro45@gmail.com>
2025-03-14 10:51:21 +0530

    Describe this commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -7,5 +7,6 @@ int main(void) {
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   cout<<"PES2UG22CS462 \n";
-  return 0;
+  cout<<"task4 \n";
+    return 0;
 }
